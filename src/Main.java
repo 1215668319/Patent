084a21b9b9dc93709eb62f528a9e1b30fe7a8cba
zyao <1215668319@qq.com>
2023-04-15 12:27:21 +0800

    Ma

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import com.sun.org.apache.xpath.internal.operations.String;
+
+import javax.print.attribute.HashAttributeSet;
+
 public class Main {
     public static void main(String[] args) {
+        if (true) {
+            new String();
+        }
         System.out.println("Hello world!");
     }
 }
